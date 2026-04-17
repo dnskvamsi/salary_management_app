34f4054df8fd72241ff99ba6eea4ac72f89a18f0
Vamsi <d.n.s.k.vamsi@gmail.com>
2026-04-17 22:23:08 +0530

    Create Employee model scaffold

---
+++ app/models/employee.rb
@@ -0,0 +1,10 @@
+# == Schema Information
+#
+# Table name: employees
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Employee < ApplicationRecord
+end
---
+++ spec/models/employee_spec.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: employees
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require 'rails_helper'
+
+RSpec.describe Employee, type: :model do
+end

b1d9981f7f1ad0776c72a2fb9e7743868eb4f93b
Vamsi <d.n.s.k.vamsi@gmail.com>
2026-04-17 22:29:04 +0530

    Add first_name to Employee table  - Configure Shouda-Matchers to spec db columns

--- spec/factories/employees.rb
+++ spec/factories/employees.rb
@@ -3,6 +3,7 @@
 # Table name: employees
 #
 #  id         :integer          not null, primary key
+#  first_name :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -3,10 +3,14 @@
 # Table name: employees
 #
 #  id         :integer          not null, primary key
+#  first_name :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
+
+  #schema
+  it { is_expected.to have_db_column(:first_name).of_type(:string) }
 end

7238280baa80eeb5cd4f29edb4c64df648805229
Vamsi <d.n.s.k.vamsi@gmail.com>
2026-04-18 13:18:17 +0530

    Add root path

--- spec/requests/employees_spec.rb
+++ spec/requests/employees_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe 'routes for employees', type: :routing do
   it { expect(get('/employees/new')).to route_to('employees#new') }
   it { expect(get('/employees/')).to route_to('employees#index') }
   it { expect(get('/employees/stats')).to route_to('employees#stats') }
+  it { expect(get('/')).to route_to('employees#index') }
 end

bb7393b801f657576a006127669f7be5d928a20c
Vamsi <d.n.s.k.vamsi@gmail.com>
2026-04-23 20:07:12 +0530

    Update seed to fetch data from first names and last names

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,13 @@ require "csv"
 Address.delete_all
 Employee.delete_all
 
-INDIAN_CITIES = %w[
+CITIES = %w[
   Mumbai Delhi Bangalore Hyderabad Chennai Kolkata Pune Ahmedabad
   Jaipur Surat Lucknow Kanpur Nagpur Indore Thane Bhopal Visakhapatnam
   Pimpri-Chinchwad Patna Vadodara Ghaziabad Ludhiana Agra Nashik
   Ranchi Faridabad Meerut Rajkot Kalyan-Dombivali Vasai-Virar
 ].freeze
+
 
 STREETS = [
   "MG Road", "Brigade Road", "Residency Road", "Infantry Road",
@@ -25,23 +26,24 @@ STREETS = [
 job_titles = Employee.job_titles.keys
 states     = Address.states.keys
 
-csv_path = Rails.root.join("db", "employees_names.csv")
+first_names_path = Rails.root.join("db", "first_names.txt")
+last_names_path = Rails.root.join("db", "last_names.txt")
 
-names = CSV.read(csv_path, headers: true).map do |row|
-    { first_name: row["first_name"], last_name: row["last_name"] }
-end
+first_names = File.readlines(first_names_path, chomp: true)
+last_names  = File.readlines(last_names_path, chomp: true)
 
 batch_size = 500
-total      = names.size
+total      = 10000
 batches    = (total.to_f / batch_size).ceil
 
 batches.times do |batch|
-    batch_names = names.slice(batch * batch_size, batch_size)
+    sampled_first_names = first_names.slice(batch * batch_size, batch_size)
+    sampled_last_names = last_names.slice(batch * batch_size, batch_size)
 
-    employees = batch_names.map do |name|
+    employees = (1..batch_size).map do |name|
     {
-        first_name: name[:first_name],
-        last_name:  name[:last_name],
+        first_name: first_names.sample,
+        last_name:  last_names.sample,
         job_title:  job_titles.sample,
         salary:     rand(300_000..2_500_000),
         created_at: Time.current,
@@ -54,12 +56,16 @@ end
 
 inserted_ids = Employee.ids
 addresses = inserted_ids.map do |employee_id|
+    country = Address.countries.keys.sample
+    state = LOCATIONS[country.to_sym].keys.sample
+
     {
     employee_id: employee_id,
     line_1:      "#{rand(1..999)}, #{STREETS.sample}",
     line_2:      "#{%w[Sector Block Phase Layout Colony].sample} #{rand(1..20)}",
-    city:        INDIAN_CITIES.sample,
-    state:       states.sample,
+    city:        CITIES.sample,
+    state:       state,
+    country:     country,
     created_at:  Time.current,
     updated_at:  Time.current
     }

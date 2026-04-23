d0a9db31e550db61b471bb2a3741fa043e1fb0f0
Vamsi <d.n.s.k.vamsi@gmail.com>
2026-04-23 19:26:04 +0530

    Add spec for Update States in Address model

---
+++ config/initializers/constants.rb
@@ -0,0 +1,64 @@
+LOCATIONS = {
+  usa: {
+    california: 0, texas: 1, florida: 2, new_york: 3, illinois: 4, 
+    pennsylvania: 5, ohio: 6, georgia: 7, north_carolina: 8, 
+    michigan: 9, washington: 10, arizona: 11
+  },
+  india: {
+    maharashtra: 12, uttar_pradesh: 13, karnataka: 14, tamil_nadu: 15, 
+    gujarat: 16, rajasthan: 17, west_bengal: 18, madhya_pradesh: 19, 
+    andhra_pradesh: 20, kerala: 21, punjab: 22, bihar: 23
+  },
+  brazil: {
+    sao_paulo: 24, rio_de_janeiro: 25, minas_gerais: 26, bahia: 27, 
+    parana: 28, rio_grande_sol: 29, pernambuco: 30, ceara: 31, 
+    santa_catarina: 32, goias: 33, amazonas: 34, mato_grosso: 35
+  },
+  australia: {
+    new_south_wales: 36, victoria: 37, queensland: 38, western_australia: 39, 
+    south_australia: 40, tasmania: 41, australian_capital_territory: 42, 
+    northern_territory: 43, jervis_bay_territory: 44, norfolk_island: 45, 
+    christmas_island: 46, cocos_keeling_islands: 47
+  },
+  germany: {
+    bavaria: 48, baden_wurttemberg: 49, north_rhine_westphalia: 50, 
+    hesse: 51, saxony: 52, lower_saxony: 53, berlin: 54, hamburg: 55, 
+    brandenburg: 56, thuringia: 57, schleswig_holstein: 58, rhineland_palatinate: 59
+  },
+  mexico: {
+    jalisco: 60, nuevo_leon: 61, estado_de_mexico: 62, veracruz: 63, 
+    puebla: 64, guanajuato: 65, chiapas: 66, oaxaca: 67, chihuahua: 68, 
+    sonora: 69, yucatan: 70, quintana_roo: 71
+  },
+  canada: {
+    ontario: 72, quebec: 73, british_columbia: 74, alberta: 75, 
+    manitoba: 76, saskatchewan: 77, nova_scotia: 78, new_brunswick: 79, 
+    newfoundland_and_labrador: 80, prince_edward_island: 81, 
+    northwest_territories: 82, yukon: 83
+  },
+  nigeria: {
+    lagos: 84, kano: 85, oyo: 86, rivers: 87, kaduna: 88, anambra: 89, 
+    delta: 90, edo: 91, enugu: 92, ogun: 93, plateau: 94, borno: 95
+  },
+  argentina: {
+    buenos_aires: 96, cordoba: 97, santa_fe: 98, mendoza: 99, 
+    tucuman: 100, salta: 101, misiones: 102, chaco: 103, 
+    corrientes: 104, neuquen: 105, chubut: 106, san_juan: 107
+  },
+  malaysia: {
+    selangor: 108, johor: 109, penang: 110, perak: 111, sabah: 112, 
+    sarawak: 113, kedah: 114, pahang: 115, kelantan: 116, 
+    terengganu: 117, negeri_sembilan: 118, melaka: 119
+  },
+  ethiopia: {
+    oromia: 120, amhara: 121, somali: 122, tigray: 123, afar: 124, 
+    sidama: 125, south_ethiopia: 126, central_ethiopia: 127, 
+    gambela: 128, benishangul_gumuz: 129, harari: 130, south_west_ethiopia: 131
+  },
+  pakistan: {
+    punjab: 132, sindh: 133, khyber_pakhtunkhwa: 134, balochistan: 135, 
+    azad_jammu_and_kashmir: 136, gilgit_baltistan: 137, 
+    islamabad_capital_territory: 138, bahawalpur: 139, khairpur: 140, 
+    kalat: 141, swat: 142, chitral: 143
+  }
+}
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -31,26 +31,5 @@ RSpec.describe Address, type: :model do
     it { should validate_presence_of(field) }
   end
 
-  it { should define_enum_for(:state).with_values(
-    andhra_pradesh: 0,
-    arunachal_pradesh: 1,
-    assam: 2,
-    bihar: 3,
-    chhattisgarh: 4,
-    goa: 5,
-    gujarat: 6,
-    haryana: 7,
-    himachal_pradesh: 8,
-    jharkhand: 9,
-    karnataka: 10,
-    kerala: 11,
-    madhya_pradesh: 12,
-    maharashtra: 13,
-    manipur: 14,
-    meghalaya: 15,
-    mizoram: 16,
-    nagaland: 17,
-    odisha: 18,
-    punjab: 19
-  )}
+  it { should define_enum_for(:state).with_values(LOCATIONS.values.inject(&:merge))}
 end
